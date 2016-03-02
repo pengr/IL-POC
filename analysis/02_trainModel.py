bfbf33f29b2150c10c65e7030d1ff9f1d73a3c89
danilnagy <kidvsking@yahoo.com>
2016-03-02 18:33:40 -0500

    update

--- analysis/02_trainModel.py
+++ analysis/02_trainModel.py
@@ -27,27 +27,7 @@ print "dataset loaded [" + str(int(time.time() - start_time)) + " sec]"
 
 random.shuffle(dataSet)
 
-## MACHINE LEARNING IMPLEMENTATION
-featureData = []
-targetData = []
-
-# use timeframe
-# for record in dataSet:
-# 		if record[1] <= 31: #limit timeframe to Jan-June
-# 			featureData.append(record[:-1])
-# 			targetData.append(record[-1])
-
-# use subsample
-# for record in dataSet[:25000]:
-# 	featureData.append(record[:-1])
-# 	targetData.append(record[-1])
-
-# use all data
-for record in dataSet:
-	featureData.append(record[:-1])
-	targetData.append(record[-1])
-
-# data dictionary
+# column reference in dataset
 # 0 - id
 # 1 - dow
 # 2 - doy
@@ -56,48 +36,70 @@ for record in dataSet:
 # 5 - cat
 # 6 - activity
 
+## MACHINE LEARNING IMPLEMENTATION
 
+# split data into feature and target sets. 
+# in this case the target of the model is the activity, which is the last column
+featureData = []
+targetData = []
+for record in dataSet:
+	featureData.append(record[:-1])
+	targetData.append(record[-1])
+
+# select which columns to include in feature set
+# in this case we are ignoring the 'id' column
 idx_IN_columns = [1, 2, 3, 4, 5] # with weather # with DOW
 
+# convert data sets to numpy arrays, and filter by columns
 X = np.asarray(featureData, dtype='float')[:,idx_IN_columns]
 y = np.asarray(targetData, dtype='float')
 
+# split data into training and testing sets
 X_train, X_test, y_train, y_test = cross_validation.train_test_split(
 	X, y, test_size=0.3, random_state=0)
+
 
 print "length of dataset: " + str(X.shape[0])
 print "length of training set: " + str(X_train.shape[0])
 print "length of test set: " + str(X_test.shape[0])
 
 
-#mean 0, variance 1
+# scale feature data to mean 0, variance 1
 scaler = preprocessing.StandardScaler().fit(X_train)
 X_train_scaled = scaler.transform(X_train)
 
-print X_train.shape
 
 
-# # CROSS VALIDATION ROUTINE
+# CROSS VALIDATION ROUTINE
 
 start_time_total = time.time()
 
+# initialize variable to store best performance number
 rs_max = 0
 
+# triple loop to iterate over range of options of 3 hyperparameters
+
+# iterate over range of 'C' variable
 for C in [1, 100, 10000]:
-# for C in [100]:
 
+	# iterate over range of 'e' variable
 	for e in [.0001, .01, 1]:
-	# for e in [1]:
 
+			# iterate over range of 'g' variable
 			for g in [.01, 1, 100]:
-			# for g in [1]:
 
 				start_time = time.time()
 
+				# set up model with current hyperparameters
 				model = svm.SVR(C=C, epsilon=e, gamma=g, kernel='rbf', cache_size=8000)
+
+				# generate model performance using cross_validation training
 				scores = cross_validation.cross_val_score(model, X_train_scaled, y_train, cv=5)
+
+				# generate average performance score
 				rs = scores.mean()
 
+				# if model score better than current best, store best model and its hyperparameters
 				if rs > rs_max:
 					rs_max = rs
 					model_best = model
@@ -112,19 +114,22 @@ print "training complete [" + str(int(time.time() - start_time_total)) + " sec]"
 print "best model: C[" + str(C_best) + "], e[" + str(e_best) + "], g[" + str(g_best) + "]"
 
 
-
 start_time_total = time.time()
 
+# fit best model using whole training set
 model_best.fit(X_train_scaled, y_train)
 
 print "training complete [" + str(int(time.time() - start_time_total)) + " sec]"
 
+# transform test feature set using stored scaler model
 X_test_scaled = scaler.transform(X_test)
+
+# generate final performance score
 rs_final = model_best.score(X_test_scaled, y_test)
 
 print "R squared (in test set): " + str(rs_final)
 
-
+# export best model and scaler for future use
 with open(workingDirectory + '\\dataModel.pkl', 'wb') as f:
 	pickle.dump(model_best, f)
 
